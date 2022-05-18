c1afacb0d51dc2b7164a37966a7ebb369b8d058f
arielBiagio <arielbiagio@gmail.com>
2022-05-18 17:24:55 -0300

    Nav y About terminados

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,56 @@
 import React from "react";
 import "./about.css";
+import me from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={me} alt="about me" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__card__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className="about__card__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__card__icon" />
+              <h5>Proyects</h5>
+              <small>80+ completed </small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia quae
+            qui velit magni ea cum eos fugit est architecto aut molestiae non
+            quidem distinctio repudiandae, odio soluta! Facere, atque delectus.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { BiMessageSquareDetail } from "react-icons/bi";
 
 function Nav() {
-  return <div>Nav</div>;
+  const [activeNav, setActiveNav] = useState("#");
+  const [show, setShow] = useState(false);
+
+  const changeShow = () => {
+    if (window.scrollY >= 60) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+  window.addEventListener("scroll", changeShow);
+
+  return (
+    <nav className={show ? "" : "hideNav"}>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
+  );
 }
 
 export default Nav;
